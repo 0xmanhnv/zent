7ee698185442ef5c29a164bc24aa2e1a3fcd443f
nguyenmanh <nguyenmanh0397@gmail.com>
2017-09-27 15:32:26 +0700

    cap nhat

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var app = express();
+var port = process.env.PORT || 8080;
+var server = require('http').Server(app);
+
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'token_facebook') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+
+//  lang nghe ket noi
+server.listen(port, function(){
+	console.log("Chat bot server listening at " + port);
+});
